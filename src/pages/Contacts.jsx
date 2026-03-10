64fd7b0d19acf061053b7aac719054ff5baa79bc
Mauro Bertaglia <msbertaglia@gmail.com>
2026-03-10 01:54:28 +0000

    File changes

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -297,7 +297,7 @@ export default function Contacts() {
       </div>
 
       {/* Content */}
-      <div className="p-6">
+      <div className="flex-1 overflow-y-auto p-6">
         {loading ? (
           <div className="flex items-center justify-center py-20">
             <div className="w-8 h-8 border-2 border-blue-500 border-t-transparent rounded-full animate-spin" />
